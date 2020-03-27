0d8f155d63e5233d2606554e2194e167c2e8edb1
VideoGameDevClub <61889676+VideoGameDevClub@users.noreply.github.com>
2020-03-27 14:56:05 -0700

    MIke's Changes

---
+++ DSS Prototype/Assets/Mike's Sample Assets/Assets/Bullet.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public GameObject hitEffect;
+
+    void Start()
+    {
+        Destroy(gameObject, 2f);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+        GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+        Destroy(effect, .1f);
+    }
+}
