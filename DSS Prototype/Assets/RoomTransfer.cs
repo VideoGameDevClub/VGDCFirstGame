7044dace9f3bbfea067422e12b7e0c1ad683cf02
AlexandruCioanca2020 <62447075+AlexandruCioanca2020@users.noreply.github.com>
2020-03-27 18:57:53 -0700

    Camera Following Player & Room Transitions & Camera Boundaries
    I made the camera follow the player's location. I did 4 points of Room Transitions (it will work better when we have the actual rooms and room sizes) and I made camera boundaries (once again...it will look better/cleaner when we actually have tiles/rooms/walls)

---
+++ DSS Prototype/Assets/RoomTransfer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTransfer : MonoBehaviour
+{
+
+    public Vector2 cameraChange;
+    public Vector3 playerChange;
+    private CameraMovement cam;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main.GetComponent<CameraMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            cam.minPosition += cameraChange;
+            cam.maxPosition += cameraChange;
+            collision.transform.position += playerChange;
+        }
+    }
+    //Change the “Camera Change” y or x position depending on how big the room is
+    //Change the “Player Change” y to 2
+    //In order to go back to the previous room, make the values that you typed above negative
+   
+    //The code works as it should be,however...
+    //We need exact measurements of the room size in order for the transitions to look nicer
+    //For now it looks wanky because I estimated a room size (8.5) just to see if it works
+}
---
+++ DSS Prototype/Assets/scripts/CameraMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform target;
+    public float Smoothing;
+
+    //boundaries
+    public Vector2 maxPosition;
+    public Vector2 minPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (transform.position != target.position)
+        {
+            //the Camera follows the Player at the X & Y positions
+            // but it keeps its own Z position
+            Vector3 targetPosition = new Vector3(target.position.x, 
+                                                 target.position.y, 
+                                                 transform.position.z);
+
+            //This following code is used to place the boundaries on the X and Y coordinates of the camera
+            //Clamp (block) the camera from seeing beyond the X position
+            targetPosition.x = Mathf.Clamp(targetPosition.x,
+                                           minPosition.x,
+                                           maxPosition.x);
+            //Clamp (block) the camera from seeing beyond the Y position
+            targetPosition.y = Mathf.Clamp(targetPosition.y,
+                                            minPosition.y,
+                                            maxPosition.y);
+
+            transform.position = Vector3.Lerp(transform.position, targetPosition, Smoothing);
+        }
+    }
+}
+
